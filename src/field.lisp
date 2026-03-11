17b4c3222ecc640bb69e747dcead5286878766b7
Park Ian Co <opensource@parkian.co>
2026-03-11 19:49:40 -0400

    Fix copyright attribution and add headers

--- src/field.lisp
+++ src/field.lisp
@@ -1,3 +1,6 @@
+;;;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
+;;;; SPDX-License-Identifier: BSD-3-Clause
+
 ;;;; field.lisp
 ;;;;
 ;;;; Finite field arithmetic for Reed-Solomon codes.
--- src/reed-solomon.lisp
+++ src/reed-solomon.lisp
@@ -1,3 +1,6 @@
+;;;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
+;;;; SPDX-License-Identifier: BSD-3-Clause
+
 ;;;; reed-solomon.lisp
 ;;;;
 ;;;; 2D Reed-Solomon erasure coding for Data Availability Sampling.
